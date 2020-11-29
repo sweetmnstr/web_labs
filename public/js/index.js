deb8bc9e4cd527164c343d752efb5aa2673fb498
Sweetmnstr <progran98@gmail.com>
2020-11-29 10:38:27 +0200

    dsdasas

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,23 @@
 new WOW().init();
+
+const regBtn = document.querySelector('#reg__form-btn')
+const phone = document.querySelector('#phone')
+const name = document.querySelector('#name')
+const email = document.querySelector('#email')
+
+
+regBtn.addEventListener('click', (e)=>{
+    e.preventDefault()
+    let dataSend = {
+        name: name.value,
+        phone: phone.value,
+        email:email.value
+    }
+    axios.post('/register', dataSend)
+        .then(data => {
+            console.log(data)
+        })
+        .catch(err => console.log(err))
+})
+
+
--- server.js
+++ server.js
@@ -2,12 +2,23 @@ const express = require("express")
 
 const app = express()
 
+const jsonparser = express.json()
+
 const port = process.env.PORT || 3000
+
+app.use(async (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
 
 app.use(express.static(__dirname + '/public'))
 
-app.use("/", ((req, res) => {
-    res.redirect('/index')
+app.post("/register",jsonparser, ((req, res) => {
+    console.log(req.body)
+    if (!req.body) res.sendStatus(400)
+    res.json(req.body)
 }))
 
 app.listen(port,()=>{
